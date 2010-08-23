8fb280012de0deb3defbe812d16b33c5c80aa505
Martyn Jago <martyn.jago@btinternet.com>
2010-08-23 13:52:55 +0100

    removed unwanted test directory

--- test_project/rakefile.rb
+++
@@ -1,9 +0,0 @@
-load '../ceedling/lib/rakefile.rb'
-
-#[:project][:build_root, 'build']
-#[:paths][:source, 'src']
-#[:paths][:test, 'test']
-
-task :default => ['test:all', :release] # ex. run all tests & build release artifact
-     # namespaced tasks must be quoted
-     # top-level tasks are Ruby symbols denoted by a leading ':'
